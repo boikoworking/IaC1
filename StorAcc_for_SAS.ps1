5aae0178cc82c6ffa52199e879e55003fe855495
boikoworking <boiko.working@gmail.com>
2022-05-23 14:28:19 +0300

    Sas token for main and parameters.
    Sas token for main and parameters.

--- StorAcc_for_SAS.ps1
+++ StorAcc_for_SAS.ps1
@@ -1,26 +1,25 @@
 Connect-AzAccount -AccountId "fopskt@gmail.com"
 Select-AzSubscription "23f5f8a4-4a06-4dce-92e7-6b980f7ad0b8"
 New-AzResourceGroup `
-  -Name ExampleGroup `
+  -Name ExampleGroup1 `
   -Location "East US"
 New-AzStorageAccount `
-  -ResourceGroupName ExampleGroup `
-  -Name depp00000 `
+  -ResourceGroupName ExampleGroup1 `
+  -Name depp00001 `
   -Type Standard_LRS `
   -Location "East US"
 Set-AzCurrentStorageAccount `
-  -ResourceGroupName ExampleGroup `
-  -Name depp00000
+  -ResourceGroupName ExampleGroup1 `
+  -Name depp00001
 New-AzStorageContainer `
-  -Name template `
+  -Name templates `
   -Permission Off
-  Set-AzStorageBlobContent `
-  -Container template `
+Set-AzStorageBlobContent `
+  -Container templates `
   -File .\main.json
-  Set-AzStorageBlobContent `
-  -Container template `
+Set-AzStorageBlobContent `
+  -Container templates `
   -File .\parametrs.json
-  # get the URI with the SAS token
 $templateuri = New-AzStorageBlobSASToken `
 -Container templates `
 -Blob main.json `
@@ -31,8 +30,7 @@ $parameteruri = New-AzStorageBlobSASToken `
 -Blob parametrs.json `
 -Permission r `
 -ExpiryTime (Get-Date).AddHours(2.0) -FullUri
-# provide URI with SAS token during deployment
 New-AzResourceGroupDeployment `
--ResourceGroupName ExampleGroup `
+-ResourceGroupName ExampleGroup1 `
 -TemplateUri $templateuri `
 -TemplateParameterUri $parameteruri
