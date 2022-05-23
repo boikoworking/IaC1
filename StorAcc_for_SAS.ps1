9d820cc13fb739c1c58557a802f12b85c7a7d2e1
boikoworking <boiko.working@gmail.com>
2022-05-23 11:58:04 +0300

    Update StorAcc_for_SAS.ps1

--- StorAcc_for_SAS.ps1
+++ StorAcc_for_SAS.ps1
@@ -2,23 +2,23 @@ Connect-AzAccount -AccountId "fopskt@gmail.com"
 Select-AzSubscription "23f5f8a4-4a06-4dce-92e7-6b980f7ad0b8"
 New-AzResourceGroup `
   -Name ExampleGroup `
-  -Location "East US"
+  -Location "Central US"
 New-AzStorageAccount `
   -ResourceGroupName ExampleGroup `
   -Name depp090909 `
   -Type Standard_LRS `
-  -Location "East US"
+  -Location "Central US"
 Set-AzCurrentStorageAccount `
   -ResourceGroupName ExampleGroup `
   -Name depp090909
 New-AzStorageContainer `
-  -Name templates `
+  -Name template `
   -Permission Off
   Set-AzStorageBlobContent `
   -Container templates `
   -File .\main.json
   Set-AzStorageBlobContent `
-  -Container templates `
+  -Container template `
   -File .\parametrs.json
   # get the URI with the SAS token
 $templateuri = New-AzStorageBlobSASToken `
