bfa10418eefde5f5653af4957f5d615f0ebb4cf4
boikoworking <boiko.working@gmail.com>
2022-05-24 12:52:27 +0300

    final

--- StorAcc_for_SAS.ps1
+++ StorAcc_for_SAS.ps1
@@ -1,4 +1,4 @@
-Connect-AzAccount -AccountId "fopskt@gmail.com"
+Connect-AzAccount
 Select-AzSubscription "23f5f8a4-4a06-4dce-92e7-6b980f7ad0b8"
 New-AzResourceGroup `
   -Name ExampleGroup1 `
@@ -11,6 +11,7 @@ New-AzStorageAccount `
 Set-AzCurrentStorageAccount `
   -ResourceGroupName ExampleGroup1 `
   -Name depp00001
+Start-Sleep -Seconds 120
 New-AzStorageContainer `
   -Name templates `
   -Permission Off
@@ -20,17 +21,13 @@ Set-AzStorageBlobContent `
 Set-AzStorageBlobContent `
   -Container templates `
   -File .\parametrs.json
-$templateuri = New-AzStorageBlobSASToken `
--Container templates `
--Blob main.json `
--Permission r `
--ExpiryTime (Get-Date).AddHours(2.0) -FullUri
-$parameteruri = New-AzStorageBlobSASToken `
--Container templates `
--Blob parametrs.json `
--Permission r `
--ExpiryTime (Get-Date).AddHours(2.0) -FullUri
-New-AzResourceGroupDeployment `
--ResourceGroupName ExampleGroup1 `
--TemplateUri $templateuri `
--TemplateParameterUri $parameteruri
+Set-AzStorageBlobContent `
+  -Container templates `
+  -File .\linked\storage_account.json
+Set-AzStorageBlobContent `
+  -Container templates `
+  -File .\linked\virtual_network.json
+$templateuri = (Get-AzStorageBlob -Container templates -Blob main.json).ICloudBlob.uri.AbsoluteUri
+$parameteruri = (Get-AzStorageBlob -Container templates -Blob parametrs.json).ICloudBlob.uri.AbsoluteUri
+$token = New-AzStorageContainerSASToken -Name templates -Permission r -ExpiryTime (Get-Date).AddMinutes(30.0)
+New-AzResourceGroupDeployment -ResourceGroupName ExampleGroup1 -TemplateUri ($templateuri + $token) -TemplateParameterUri ($parameteruri + $token) -containerSasToken $token
